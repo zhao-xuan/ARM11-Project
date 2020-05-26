741d321691cbc33e171f3a3384cadd4169966917
danieldeng2 <danieldeng223@gmail.com>
2020-05-26 14:46:36 +0100

    finished read binary file module.

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,1 +1,57 @@
 #include "utils.h"
+
+int readBinaryFile(char *path, uint32_t **buffer, int *size) {
+  FILE *file;
+  size_t fileLen;
+
+  file = fopen(path, "rb");
+  if (!file) {
+    fprintf(stderr, "Unable to open file %s", path);
+    return -1;
+  }
+
+  fseek(file, 0, SEEK_END);
+  fileLen = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  *size = fileLen + 1;
+  *buffer = (uint32_t *)malloc(*size);
+
+  if (!*buffer) {
+    fprintf(stderr, "Memory error!");
+    fclose(file);
+    return -1;
+  }
+  fread(*buffer, fileLen, 1, file);
+  fclose(file);
+  return 0;
+}
+
+void printBits(uint32_t x) {
+  int i;
+  uint32_t mask = 1 << 31;
+  for (i = 0; i < 32; ++i) {
+    if ((x & mask) == 0) {
+      printf("0");
+    } else {
+      printf("1");
+    }
+    x = x << 1;
+  }
+  printf("\n");
+}
+
+void dump_buffer(uint32_t *buffer, int size) {
+  for (int i = 0; i < size; i++) printBits(buffer[i]);
+}
+
+int main(void){
+		uint32_t *buffer = NULL;
+		int size;
+		if (readBinaryFile("./hello", &buffer, &size) == 0) {
+			printf("%d\n", size);
+			dump_buffer(buffer, size);
+			free(buffer);
+  	}
+	return 0;
+}
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -5,7 +5,46 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
+/*
+	@brief: pass by value-result. REMEMBER TO FREE buffer when done. 
+	Takes in path to a file, returns content of the file, and size of the file. 
+	@param: 
+		*path: path to the required file. 
+		**buffer: an empty pointer, will point to the content of the file when done. 
+		*size: an empty pointer, the value will be set to the size of the file when done. 
+	@returns: -1 if an error occurred, and 0 if OK. 
+	@example: 
+		uint32_t *buffer = NULL;
+		int size;
+		if (ReadBinaryFile("./hello", &buffer, &size) == 0) {
+			printf("%d\n", size);
+			dump_buffer(buffer, size);
+			free(buffer);
+  	}
+*/
+int readBinaryFile(char *path, uint32_t **buffer, int *size);
+
+
+/*
+	@brief: takes in an unsigned 32 bit number, and print out its bits. 
+	@param: 
+		x: the number to be printed. 
+*/
+void printBits(uint32_t x);
+
+
+
+/*
+	@brief: takes in an array of 32 bit numbers, and print out its bits. 
+	@param: 
+		buffer: the buffer to be dumped. 
+		size: the size of the buffer. 
+*/
+void dump_buffer(uint32_t *buffer, int size);
 
 
 #endif
