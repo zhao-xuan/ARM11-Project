4ba9fddd8baa83636ddae75abcdca8f5f3d0e8f3
zhao-xuan <1418756682@qq.com>
2020-05-28 10:29:11 +0800

    decode debugging functions added

--- src/emulate/decode.c
+++ src/emulate/decode.c
@@ -4,6 +4,9 @@ static void data_processing_helper(word_t binary, instruction_t *struct_p, data_
 static void multiply_helper(word_t binary, instruction_t *struct_p, multiply_t *instr_p);
 static void data_transfer_helper(word_t binary, instruction_t *struct_p, data_transfer_t *instr_p);
 static void branch_helper(word_t binary, instruction_t *struct_p, branch_t *instr_p);
+/* The two functions below are for debugging purpose*/
+static void print_decoded(word_t binary, instruction_t *instr);
+static void print_bits(uint32_t x, size_t size);
 
 instruction_t *decode(const word_t binary) {
     /*
@@ -43,6 +46,8 @@ instruction_t *decode(const word_t binary) {
             fprintf(stderr, "Instruction type doesn't match!");
             break;
     }
+
+    print_decoded(binary, instr_struct);
 
     return instr_struct;
 }
@@ -113,4 +118,60 @@ static void branch_helper(word_t binary, instruction_t *struct_p, branch_t *inst
     instr_p -> cond = (binary >> 28) & 0xf;
     instr_p -> offset = binary & 0xffffff;
     (struct_p -> instructions).branch = *instr_p;
+}
+
+static void print_decoded(word_t binary, instruction_t *instr) {
+    /* This function is written for debugging */
+    print_bits(binary, 32);
+    if (instr -> type == DATA_PROCESSING) {
+        printf("data processing\n");
+        printf("cond: "); print_bits(instr -> instructions.data_processing.cond, 4);
+        printf("rn: %d\n", instr -> instructions.data_processing.rn);
+        printf("rd: %d\n", instr -> instructions.data_processing.rd);
+        printf("opcode: "); print_bits(instr -> instructions.data_processing.opcode, 4);
+        printf("operand2: %d\n", instr -> instructions.data_processing.operand2);
+        printf("immconst: %d\n", instr -> instructions.data_processing.imm_const);
+        printf("set: %d\n\n", instr -> instructions.data_processing.set);
+    } else if (instr -> type == MULTIPLY) {
+        printf("multiply\n");
+        printf("cond: "); print_bits(instr -> instructions.multiply.cond, 4);
+        printf("rm: %d\n", instr -> instructions.multiply.rm);
+        printf("rd: %d\n", instr -> instructions.multiply.rd);
+        printf("rs: %d\n", instr -> instructions.multiply.rs);
+        printf("rn: %d\n", instr -> instructions.multiply.rn);
+        printf("accumulate: %d\n", instr -> instructions.multiply.accumulate);
+        printf("set: %d\n\n", instr -> instructions.multiply.set);
+    } else if (instr -> type == DATA_TRANSFER) {
+        printf("data_transfer\n");
+        printf("offset: %d", instr -> instructions.data_transfer.offset);
+        printf("cond: "); print_bits(instr -> instructions.data_transfer.cond, 4);
+        printf("rn: %d\n", instr -> instructions.data_transfer.rn);
+        printf("rd: %d\n", instr -> instructions.data_transfer.rd);
+        printf("imm_offset: %d\n", instr -> instructions.data_transfer.imm_offset);
+        printf("pre_index: %d\n", instr -> instructions.data_transfer.pre_index);
+        printf("up_bit: %d\n", instr -> instructions.data_transfer.up_bit);
+        printf("load: %d\n\n", instr -> instructions.data_transfer.load);
+    } else if (instr -> type == BRANCH) {
+        printf("branch\n");
+        printf("offset: %d\n", instr -> instructions.branch.offset);
+        printf("cond: "); print_bits(instr -> instructions.branch.cond, 4);
+    }
+}
+
+static void print_bits(uint32_t x, size_t size) {
+    /* This function is written for debugging */
+    int i;
+    uint32_t mask = 1 << (size - 1);
+    for (i = 0; i < size; ++i) {
+        if ((x & mask) == 0) {
+            printf("0");
+        } else {
+            printf("1");
+        }
+        if ((i + 1) % 4 == 0) {
+            printf(" ");
+        }
+        x = x << 1;
+    }
+    printf("\n");
 }
