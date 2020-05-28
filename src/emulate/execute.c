85b977e98d48f4d693e4686d1cc2b794888a59f0
zhao-xuan <1418756682@qq.com>
2020-05-28 13:00:04 +0800

    execute outline finished

--- src/emulate/execute.c
+++ src/emulate/execute.c
@@ -1,1 +1,16 @@
 #include "execute.h"
+
+word_t fetch_instruction(state_t state) {
+    word_t pre_instruction = state.fetched_instruction;
+    state.fetched_instruction = state.memory[get_reg(PC)];
+    set_reg(PC, get_reg(PC) + 4);
+    return pre_instruction;
+}
+
+instruction_t decode_instruction(state_t state, word_t pre_instruction) {
+    instruction_t *decoded_instruction = decode(pre_instruction);
+
+}
+
+int execute_instruction(state_t state);
+int pipeline(state_t state);
--- src/emulate/include/execute.h
+++ src/emulate/include/execute.h
@@ -9,4 +9,37 @@
 #include "instructions.h"
 #include "global.h"
 
+/*
+ * @brief: fetch a line of instruction to state_t
+ * @param: state_t state: memory and registers
+ * @return: the previous instruction stored in state_t
+ */
+
+word_t fetch_instruction(state_t state);
+
+/*
+ * @brief: put the decoded instruction in state_t
+ * @param: state_t state: memory and registers
+ * @return: the previous decoded instruction stored in state_t
+ */
+
+instruction_t decode_instruction(state_t state);
+
+/*
+ * @brief: execute the decoded instruction
+ * @param: state_t state: memory and registers
+ * @return: 0 if instruction is executed successfully, -1 if error occurs
+ */
+
+int execute_instruction(state_t state);
+
+/*
+ * @brief: simulate a pipeline of ARM processor
+ *         pipiline() will intergrate the three functions above
+ * @param: state_t state: memory and registers
+ * @return: 0 if instruction is executed successfully, -1 if error occurs
+ */
+
+int pipeline(state_t state);
+
 #endif
