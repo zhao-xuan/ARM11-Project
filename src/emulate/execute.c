b9257bd67f21189465d58aad5f07237c6e88e6e6
zhao-xuan <1418756682@qq.com>
2020-05-28 18:40:25 +0800

    8-implement-execute skeleton finished.

--- src/emulate/execute.c
+++ src/emulate/execute.c
@@ -1,16 +1,88 @@
 #include "execute.h"
 
-word_t fetch_instruction(state_t state) {
-    word_t pre_instruction = state.fetched_instruction;
+void fetch_instruction(state_t state) {
     state.fetched_instruction = state.memory[get_reg(PC)];
     set_reg(PC, get_reg(PC) + 4);
-    return pre_instruction;
 }
 
-instruction_t decode_instruction(state_t state, word_t pre_instruction) {
-    instruction_t *decoded_instruction = decode(pre_instruction);
-
+void decode_instruction(state_t state) {
+    state.decoded_instruction = *(decode(state.fetched_instruction));
+    fetch_instruction(state);
 }
 
-int execute_instruction(state_t state);
-int pipeline(state_t state);
+int execute_instruction(state_t state) {
+    intruction_t instr_to_exec = state.decoded_instruction;
+    decode_instruction(state);
+    execute();
+}
+
+int pipeline(state_t state) {
+    while (true) {
+        fetch_instruction(state);
+        decode_instruction(state);
+        execute_instruction(state);
+    }
+}
+
+int execute(instruction_t instr_to_exec) {
+    switch (instr_to_exec.type) {
+        case DATA_PROCESSING:
+            data_processing_execute(instr_to_exec.instructions.data_processing);
+            break;
+        case MULTIPLY:
+            multiply_execute(instr_to_exec.instructions.multiply);
+            break;
+        case DATA_TRANSFER:
+            data_transfer_execute(instr_to_exec.instructions.data_transfer);
+            break;
+        case BRANCH:
+            branch_execute(instr_to_exec.instructions.branch);
+            break;
+        default:
+            fprintf(stderr, "Instruction Type Error!");
+    }
+
+    return 0;
+}
+
+/* Below are helper functions of executing different types of instructions.*/
+
+/*
+ * @param: data_processing_t dp_instr: the representation of a data processing instruction
+ * @return: 0 if succeeded, -1 if error has occurred
+ */
+static int data_processing_execute(data_processing_t dp_instr) {
+    /* Data processing instructions should be executed here */
+
+    return 0;
+}
+
+/*
+ * @param: multiply_t dp_instr: the representation of a multiply instruction
+ * @return: 0 if succeeded, -1 if error has occurred
+ */
+static int multiply_execute(multiply_t mul_instr) {
+    /* Multiply instructions should be executed here */
+
+    return 0;
+}
+
+/*
+ * @param: data_transfer dp_instr: the representation of a data transfer instruction
+ * @return: 0 if succeeded, -1 if error has occurred
+ */
+static int data_transfer_execute(data_transfer_t dt_instr) {
+    /* Data transfer instructions should be executed here */
+
+    return 0;
+}
+
+/*
+ * @param: branch_t dp_instr: the representation of a branch instruction
+ * @return: 0 if succeeded, -1 if error has occurred
+ */
+static int branch_execute(data_processing_t dp_instr) {
+    /* Branch instructions should be executed here */
+
+    return 0;
+}
--- src/emulate/include/execute.h
+++ src/emulate/include/execute.h
@@ -15,7 +15,7 @@
  * @return: the previous instruction stored in state_t
  */
 
-word_t fetch_instruction(state_t state);
+void fetch_instruction(state_t state);
 
 /*
  * @brief: put the decoded instruction in state_t
@@ -23,10 +23,10 @@ word_t fetch_instruction(state_t state);
  * @return: the previous decoded instruction stored in state_t
  */
 
-instruction_t decode_instruction(state_t state);
+void decode_instruction(state_t state);
 
 /*
- * @brief: execute the decoded instruction
+ * @brief: execute the decoded instruction in the pipeline and fetch/decode new ones
  * @param: state_t state: memory and registers
  * @return: 0 if instruction is executed successfully, -1 if error occurs
  */
@@ -35,11 +35,19 @@ int execute_instruction(state_t state);
 
 /*
  * @brief: simulate a pipeline of ARM processor
- *         pipiline() will intergrate the three functions above
+ *         pipeline() will integrate the three functions above
  * @param: state_t state: memory and registers
  * @return: 0 if instruction is executed successfully, -1 if error occurs
  */
 
 int pipeline(state_t state);
+
+/*
+ * @brief: execute a single instruction
+ * @param instruction_t instr: a single instruction structure
+ * @return: 0 if succeeded, -1 if error has occurred
+ */
+
+int execute(instruction_t instr);
 
 #endif
