05e6b054cf3e52a434ed8d9fee5386def2705060
Hoangvu <vuhoang1609@gmail.com>
2020-05-29 10:00:03 +0100

    Removed excess codes from header file

--- src/emulate/include/logicunit.h
+++ src/emulate/include/logicunit.h
@@ -12,50 +12,16 @@
 #include "global.h"
 #include "state.h"
 
-
-/*
- *  The following functions define require ALU operations
- *  required for Data-Processing-Instructions for part 1
- *  
- *  @param: 
- *    op1, op2: the 2 operands 
- *    cout: pointer to write the carry-out bit into
- *    res: pointer to where to write the result into
- *  
- *  @execution:
- *    If you pass in a null pointer, it will only write the carry out 
- *    *res = op1 <operator> op2
- *      
- *    *cout = see the description in the spec for C-bit 
- *    
- *      NOTICE that this Carryout is ONLY responsible for the C-bit
- *      of the instruction currently being executed
- */
-static void and(word_t op1, word_t op2, bool *cout, word_t *res);
-static void eor(word_t op1, word_t op2, bool *cout, word_t *res);
-static void sub(word_t op1, word_t op2, bool *cout, word_t *res);
-static void rsb(word_t op1, word_t op2, bool *cout, word_t *res);
-static void add(word_t op1, word_t op2, bool *cout, word_t *res);
-static void orr(word_t op1, word_t op2, bool *cout, word_t *res);
-static void mov(word_t op1, word_t op2, bool *cout, word_t *res);
-
-
-static void lsl(byte_t shamt, word_t op2, bool *cout, word_t *res);
-static void lsr(byte_t shamt, word_t op2, bool *cout, word_t *res);
-static void asr(byte_t shamt, word_t op2, bool *cout, word_t *res);
-static void ror(byte_t shamt, word_t op2, bool *cout, word_t *res);
-
-
 /* 
- *  Make a call to the ALU
+ *  Make a call to the ALU and Barrel Shifter
  *  
  *  @param: op1, op2, opcode, set_flag
- *      set_flag: whether or not to set flags NZC in the CPSR
+ *      set: whether or not to set flags NZC in the CPSR
  *
  *
  *  @return: exit status
- *      0 - if everything goes well
- *      1 - Unknown opcode as defined at the top
+ *            0        - if everything goes well
+ *      UNKNOWN_OPCODE - Unknown opcode as defined at the top
  */
 int alu(word_t op1, word_t op2, byte_t opcode, bool set);
 
