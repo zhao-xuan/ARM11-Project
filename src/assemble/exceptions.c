b5656af3f1537e610b026480ef02468dec3340f7
Kaiyan Fan <kf619@ic.ac.uk>
2020-06-09 14:17:41 +0100

    Update exception handling to show the address of problem.

--- src/assemble/exceptions.c
+++ src/assemble/exceptions.c
@@ -4,10 +4,11 @@
  * But skeletons for exception handling are provided so it can be easily
  * implemented if needed.
  */
+#include <stdio.h>
 #include <stdlib.h>
 #include "exceptions.h"
 
 void exceptions(byte_t error_code, address_t current) {
-  fprintf("Unhandled exception! Error code %d\n", error_code);
+  fprintf(stderr, "Unhandled exception! Error code %d at address 0x%08x\n", error_code, current);
   exit(EXIT_FAILURE);
 }
