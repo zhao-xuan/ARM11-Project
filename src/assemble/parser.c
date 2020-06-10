4b42b0f7d26bc86c87cddce518baca0732d11db7
zhao-xuan <1418756682@qq.com>
2020-06-10 16:51:49 +0800

    data_processing 75% finished: finished skeleton and operand2 parsing, still need testing and error handling, and immediate processing

--- src/assemble/parser.c
+++ src/assemble/parser.c
@@ -29,25 +29,61 @@ void free_machine_code(machine_code *mcode) {
 // Implementation for the parser helper functions below:
 /* DATA_PROCESSING parsing functions*/
 static void parse_dp(assembly_line *line, word_t *bin) {
-  byte_t opcode_field = (bin >> OPCODE_LOCATION) & FOUR_BIT_FIELD;
+  byte_t opcode_field = (*bin >> OPCODE_LOCATION) & FOUR_BIT_FIELD;
   char **tokens = operand_processor(line->operands);
   if (opcode_field <= 4 || opcode_field == 12) {
     /* result-computing instruction */
-    bin = bin | ();
+    *bin |= to_index(tokens[0]) << DP_DT_RD_LOCATION;
+    *bin |= to_index(tokens[1]) << DP_DT_RN_LOCATION;
+    *bin |= parse_dp_operand2(tokens[2]); //parse_dp_operand2 will take care of the immediate bit as well
   } else if (opcode_field == 13) {
     /* mov instruction */
-    
+    *bin |= to_index(tokens[0]) << DP_DT_RD_LOCATION;
+    *bin |= parse_dp_operand2(tokens[1]); //parse_dp_operand2 will take care of     the immediate bit as well
   } else if (opcode_field >= 8 && opcode_field <= 10) {
     /* CPSR flag set instruction */
+    *bin |= to_index(tokens[0]) << DP_DT_RN_LOCATION;
+    *bin |= parse_dp_operand2(tokens[1]); //parse_dp_operand2 will take care of     the immediate bit as well
   }
 }
 
 static wort_t parse_dp_operand2(char *operand2) {
-  
-}
+  word_t bin = 0;
+  if (operand2[0] == '#') {
+    bin |= 1 << IMM_LOCATION;
+    long imm = to_index(operand2);
+    if (imm >= 0 && imm < 128)  {
+      bin |= imm;
+    } else {
+      /* where the right rotation is needed */
+    }
+  } else {
+    char *base_reg = strtok(operand2, ",");
+    bin |= to_index(base_reg);
+    char *shift_name = strtok(NULL, " ");
+    char *shift_name_list[] = {"lsl", "lsr", "asr", "ror"};
+    for (int i = 0; i < 4; i++) {
+      if (strcmp(shift_name_list[i], shift_name)) {
+          bin |= i << OPERAND2_SHIFT_TYPE_LOCATION;
+          break;
+      }
+    }
 
-static byte_t reg_to_bin(char *reg) {
-  return atoi(reg + 1);
+    char *shamt_str = strtok(NULL, " ");
+    long shamt = to_index(shamt_str);
+    if (shamt_str[0] == '#') {
+      if (shift_integer < 32) {
+        bin |= shift_integer << OPERAND2_INTEGER_SHIFT_LOCATION;
+      } else {
+        /* An error should be thrown: shift integer is not in the range */
+      }
+    } else {
+      bin |= 1 << OPERAND2_SHIFT_SPEC_LOCATION;
+      bin |= shamt << OPERAND2_REGISTER_SHIFT_LOCATION;
+    }
+  }
+
+  return bin;
 }
 
 // Implementation for the string processing (helper) functions below:
@@ -57,13 +93,13 @@ static byte_t reg_to_bin(char *reg) {
  * @return: an array of string representing the operand fields
  */
 
-#define MAX_FIELD_COUNT 4;
+#define MAX_OPERAND2_FIELD_COUNT 3;
 
 static char **operand_processor(char *operand) {
-  char **tokens = calloc(MAX_FIELD_COUNT, sizeof(char *));
+  char **tokens = calloc(MAX_OPERAND2_FIELD_COUNT, sizeof(char *));
   int i = 0;
   char *literal = strtok(operand, ",");
-  while (literal != NULL) {
+  while (literal != NULL || i < MAX_OPERAND2_FIELD_COUNT) {
     tokens[i] = malloc(strlen(literal) * sizeof(char));
     strcpy(token[i], literal);
     i++;
