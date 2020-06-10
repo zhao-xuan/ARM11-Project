b49d4a669779a39fcaf8e34f21a200262258978c
Kaiyan Fan <kf619@ic.ac.uk>
2020-06-10 04:56:42 +0100

    Write the general structure for parse function

--- src/assemble/parser.c
+++ src/assemble/parser.c
@@ -5,12 +5,22 @@
 
 // Implementation for public functions:
 machine_code *parse(assembly_program *program, symbol_table_t *label_table) {
+  machine_code *mcode = malloc(sizeof(machine_code));
+  mcode->length = program->total_lines;
+  mcode->bin = calloc(mcode->length, sizeof(word_t));
+
+  for (int i = 0; i < program->total_lines; i++) {
+    assembly_line *line = program->lines[i];
+    mnemonic_p content = get_mnemonic_data(line->opcode);
+    word_t bin = content->bin;
+    
+  }
   // TODO
   return NULL;
 }
 
 void free_machine_code(machine_code *mcode) {
-  free(mcode->bin);
+  free(mcode -> bin);
   free(mcode);
 }
 
