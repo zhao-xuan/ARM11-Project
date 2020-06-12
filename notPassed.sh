6a7b1352a6d6860a29760e477d0d7bd20a6bf78d
hhv19@Inspiron <hhv19@ic.ac.uk>
2020-06-12 13:03:17 +0100

    Added memory test script

--- notPassed.sh
+++ notPassed.sh
@@ -1,12 +1,22 @@
+ASSEMBLE=../../arm11_04/bin/assemble
+EMULATE=../../arm11_04/bin/emulate
+
+mkdir -p analysis/emulator/memory; mkdir -p analysis/emulator/profiler; 
+mkdir -p analysis/assembler/memory; mkdir -p analysis/assembler/profiler; 
+
 make clean all &>/dev/null; 
 shopt -s extglob;
 cd ../arm11_testsuite/test_cases/;
 for file in !(*.*); do
-	../../arm11_04/bin/assemble $file.s /tmp/$file &>/dev/null;
+	$ASSEMBLE $file.s /tmp/$file &>/dev/null;
 	if ! cmp -s "/tmp/$file" "$file" ; then
 		printf "\n\n$file DIFF:\n"
 		xxd -b "/tmp/$file" > "/tmp/$file.bin"
 		xxd -b "$file" > "/tmp/their$file.bin"
 		diff "/tmp/$file.bin" "/tmp/their$file.bin"
 	fi
+
+  valgrind --leak-check=full $ASSEMBLE $file.s /tmp/$file &> ../../arm11_04/analysis/assembler/memory/$file.memtest.txt
+  valgrind --leak-check=full $EMULATE $file &> ../../arm11_04/analysis/emulator/memory/$file.memtest.txt
 done
+
